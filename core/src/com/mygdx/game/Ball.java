8d437b679a96719c3ec38e148eeec7e79a9a2d26
rigidus <i.am.rigidus@gmail.com>
2017-10-05 20:09:39 +0300

    .10

--- core/src/com/mygdx/game/Ball.java
+++ core/src/com/mygdx/game/Ball.java
@@ -9,10 +9,10 @@ public class Ball {
     public Vector2 pos;
     public Vector2 speed;
     
-    public Ball (String imgFileName, int x, int y) {
+    public Ball (String imgFileName, int x, int y, int speed_x, int speed_y) {
         img = new Texture(imgFileName);
         pos = new Vector2(x, y);
-        speed = new Vector2(0,0);
+        speed = new Vector2(speed_x,speed_y);
     }
     
     public void move (Vector2 maxpos) {
@@ -22,7 +22,7 @@ public class Ball {
         if (speed.x > 7) speed.x = 7;
         if (speed.y > 7) speed.y = 7;
         if (pos.x < 0) { speed.x = -speed.x; pos.x = 0; }
-        if (pos.y < 0) { speed.y = -speed.y; pos.y = 0; }
+        //if (pos.y < 0) { speed.y = -speed.y; pos.y = 0; }
         if (pos.x > maxpos.x - img.getWidth())  { speed.x = -speed.x; pos.x = maxpos.x - img.getWidth();  }
         if (pos.y > maxpos.y - img.getHeight()) { speed.y = -speed.y; pos.y = maxpos.y - img.getHeight(); }
     }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -12,6 +12,10 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Ball ball;
 	Texture gameover;
+	Texture platform;
+	Texture youwin;
+	Vector2 speed;
+	Vector2 pos;
 
 	int brksCount = 7;
 	Brick[] brks = new Brick[brksCount];
@@ -20,12 +24,17 @@ public class MyGdxGame extends ApplicationAdapter {
 	Vector2 oldPos = new Vector2(0,0);
 
 	boolean bGameOver = false;
+	boolean bWin = false;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		ball = new Ball("ball.png", 0, 0);
+		ball = new Ball("ball.png", 200, 200, 1, 1);
 		gameover = new Texture("gameover.jpg");
+		platform = new Texture("platform.png");
+		youwin = new Texture("youwin.png");
+		speed = new Vector2(0,0);
+		pos = new Vector2(0,0);
 		// make wall
 		for (int i=0; i<brksCount; i++) {
 			int y = 300;
@@ -43,36 +52,107 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		// control
-		if (Gdx.input.isKeyPressed(Input.Keys.UP))    { ball.speed.y += 1; }
-		if (Gdx.input.isKeyPressed(Input.Keys.DOWN))  { ball.speed.y -= 1; }
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT))  { ball.speed.x -= 1; }
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) { ball.speed.x += 1; }
+		if (!bGameOver) {
 
-		// move ball
-		ball.move(maxPos);
-
-		// collision detection and reaction
-		for (Brick brk : brks) {
-			if (brk.isCollision(ball.pos, ball.img)) {
-				oldPos.set(ball.pos.x - 2 * ball.speed.x, ball.pos.y - 2 * ball.speed.y);
-				if      (brk.isCollisionHorizontal(oldPos, ball.img))   ball.speed.y = - ball.speed.y;
-				else if (brk.isCollisionVertical  (oldPos, ball.img))   ball.speed.x = - ball.speed.x;
-				else {                                                  ball.speed.x = - ball.speed.x; ball.speed.y = - ball.speed.y; }
-				// out of the box
-				ball.pos.x = oldPos.x;
-				ball.pos.y = oldPos.y;
-				// delete brick
-				brk.pos.y = - brk.pos.y;
+			// control
+			if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+				speed.x -= 0.3;
 			}
-		}
+			if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+				speed.x += 0.3;
+			}
 
-		// condition of end game
-		bGameOver = true;
-		for (Brick brk : brks) {
-			if (brk.pos.y > 0) {
-				bGameOver = false;
-				break;
+			// move ball
+			ball.move(maxPos);
+
+			if (speed.x != 0) {
+				pos.x = pos.x + 2 * speed.x;
+			} // Рассчитаем следующую позицию платформы
+			if (speed.x > 7) {
+				speed.x = 7;
+			} // Негуманно двигать платформу быстрее
+			if (pos.x < 0) {
+				speed.x = 0;
+				pos.x = 0;
+			} // Тормозим платформу об левую стену
+			if (pos.x > Gdx.graphics.getWidth() - platform.getWidth()) { // Тормозим платформу об правую стену
+				speed.x = 0;
+				pos.x = Gdx.graphics.getWidth() - platform.getWidth();
+			}
+
+			// Проверяем все кирпичи на предмет столкновения с шариком
+			for (Brick brk : brks) {
+				if (brk.isCollision(ball.pos, ball.img)) {
+					oldPos.set(ball.pos.x - 2 * ball.speed.x, ball.pos.y - 2 * ball.speed.y);
+					if (brk.isCollisionHorizontal(oldPos, ball.img)) ball.speed.y = -ball.speed.y;
+					else if (brk.isCollisionVertical(oldPos, ball.img)) ball.speed.x = -ball.speed.x;
+					else {
+						ball.speed.x = -ball.speed.x;
+						ball.speed.y = -ball.speed.y;
+					}
+					// out of the box
+					ball.pos.x = oldPos.x;
+					ball.pos.y = oldPos.y;
+					// Так как мы не можем простым способом удалить кирпич из массива -
+					// меняем ему координату высоты на отрицательную
+					brk.pos.y = -brk.pos.y;
+				}
+			}
+
+			// Условие завершения с выигрышем - нет кирпичей с положительной высотой
+			bWin = true;
+			for (Brick brk : brks) {
+				if (brk.pos.y > 0) {
+					bWin = false;
+					break;
+				}
+			}
+
+			// Если игрок выиграл, то игра определенно закончилась
+			if (bWin) {
+				bGameOver = true;
+			}
+
+			// Работа с отбиванием шарика
+			if (ball.pos.y < platform.getHeight()) {
+				// Шарик ниже платформы - это наш случай
+				if ((ball.pos.x > pos.x) && (ball.pos.x + ball.img.getWidth() < pos.x + platform.getWidth())) {
+					// Игрок успел подвести платформу. Начинаем расчет угла отражения шарика.
+
+					// Обратим вектор вертикальной скорости
+					ball.speed.y = -ball.speed.y;
+
+					// Вычислим центр шарика
+					double ballCenter = ball.pos.x + ball.img.getWidth();
+					// Вычислим центр платформы
+					double platformCenter = pos.x + platform.getWidth();
+					// Расстояние между центром шара центром платформы
+					double ro = ballCenter - platformCenter;
+
+					// Если значение шара плюс-минус небольшая величина, то отражаем с тем же углом, с каким упал
+					if (Math.abs(ro) < 20) {
+						// т.е. не изменяем горизонтальный вектор скорости
+//					} else if (Math.abs(ro) > 18) {
+//						// Если слишком большая - инвертируем горизонтальную скорость
+//						ball.speed.x *= -1;
+					} else {
+						// В ином случае
+
+//						double cx = ball.speed.x;
+//						double cy = ball.speed.y;
+//						// Вычисляем угол падения
+//						double alfa = Math.PI * 0.2 * (ro) / 18;
+//						// Вычисляем угол отражения
+//						ball.speed.x = (float) (cx * Math.cos(alfa) - cy * Math.sin(alfa));
+//						ball.speed.y = (float) (cy * Math.cos(alfa) - cx * Math.sin(alfa));
+					}
+
+					// Скорректируем положение шарика относительно платформы
+					ball.pos.y = platform.getHeight();
+				} else {
+					// Игрок промахнулся - игре конец
+					bGameOver = true;
+				}
 			}
 		}
 
@@ -83,14 +163,24 @@ public class MyGdxGame extends ApplicationAdapter {
 			for (Brick brk : brks) {
 				brk.render(batch);
 			}
+			batch.draw(platform, pos.x, pos.y);
 		} else {
 			// game over
-			batch.draw(gameover,
-					maxPos.x/2 - gameover.getWidth()/2,
-					maxPos.y/2 - gameover.getHeight()/2);
+			if (bWin) {
+				// win
+				batch.draw(youwin,
+						Gdx.graphics.getWidth() / 2 - youwin.getWidth() / 2,
+						Gdx.graphics.getHeight() / 2 - youwin.getHeight() / 2);
+			} else {
+				// lose
+				batch.draw(gameover,
+						Gdx.graphics.getWidth() / 2 - gameover.getWidth() / 2,
+						Gdx.graphics.getHeight() / 2 - gameover.getHeight() / 2);
+			}
 		}
 		batch.end();
 	}
+
 
 	@Override
 	public void dispose () {
@@ -100,5 +190,6 @@ public class MyGdxGame extends ApplicationAdapter {
 			brk.dispose();
 		}
 		gameover.dispose();
+		youwin.dispose();
 	}
 }
