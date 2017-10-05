50b3a4faa199426c64ab2210e70c17cf9b25e8b4
rigidus <i.am.rigidus@gmail.com>
2017-10-05 20:05:06 +0300

    .2

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,11 +6,12 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	boolean pressed = false;                           //
+	Vector2 position = new Vector2(10, 0); // *
 
 	@Override
 	public void create () {
@@ -20,17 +21,14 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		if (Gdx.input.isKeyPressed(Input.Keys.UP))    { position.y += 10; } // *
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN))  { position.y -= 10; } // *
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT))  { position.x -= 10; } // *
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) { position.x += 10; } // *
 		batch.begin();
-		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {  //
-			this.pressed = true;                         //
-		}                                                //
-		if (!pressed) {                                  //
-			batch.draw(img, 100, 100);              //
-		} else {                                         //
-			batch.draw(img, 0, 0);                  //
-		}                                                //
+		batch.draw(img, position.x, position.y);
 		batch.end();
 	}
 
